d4de4a51ac5c88b1502c16f5485a2f0d83b02302
ArinaRakhaeva <raxaevaa@bk.ru>
2020-05-02 20:14:58 +0300

    Added formating and design elements.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WebTech
 Project for a Web Tachnologies course
+
+It's an online tool for ordering pre-cooked food. 
+The main functionality is contained in main.js. Every meal plan has a value that changes depending on what area and time you choose. The end value output is contained in the "price" section. 
--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,145 +1,76 @@
-const plansList = document.querySelector('#plans');
-const areasList = document.querySelector('#areas');
-const pricesList = document.querySelector('#prices');
+var prices = {
+    "plan" : {
+        "fit" : 1000,
+        "normal" : 2000,
+        "family" : 3000,
+        "" : 0,
+    },
+    "area" : {
+        "Kalininsky" : 1000,
+        "Central" : 2000,
+        "VO" : 3000,
+        "" : 0,
+    },
+    "time" : {
+        "AM" : 1000,
+        "PM" : 2000,
+        "" : 0,
+    }
+};
 
-let filteredData = JSON.parse(JSON.stringify(data));
+var options = {
+    "plan" : "",
+    "area" : "",
+    "time" : "",
+};
 
-const domManipulator = (function () {
-    return {
-        generateList: function (array) {
-//let объявдяем переменную html-пустая строка
-            let html = '';
-            //на вход-массив, на выход-строчка
-            array.forEach(li => {
-                html += '<li>'.concat(li).concat('</li>');
-            });
-            return html;
-        },
-        //обновляя страничку, вставляем li
-        updateNode: function (node, content, where = "afterBegin") {
-            node.insertAdjacentHTML(where, content);
-        },
-        clearNodes: function (...nodes) {
-            nodes.forEach(el => {
-                el.innerHTML = '';
-            })
+var resultPrice = 0;
+
+function changeStylePlans(element) {
+    let planes = [document.getElementById("fit"),
+        document.getElementById("normal"),
+        document.getElementById("family")];
+    planes.forEach(plan => {
+        if (plan === element) {
+            plan.classList.add("btn-primary");
+            options["plan"] = plan.id;
+        } else {
+            plan.classList.remove("btn-primary");
         }
-    }
-})();
-
-const dataManipulator = (function (data) {
-    return {
-        data: data.plans,
-        getAllShops: function (data) {
-            //возвращает массив магазинов
-            const plans = data && data.plans || this.data;
-            return plans.map(el => el.name);
-        },
-        getAllFlowers: function (data) {
-            const areas = [];
-            const plans = data && data.plans || this.data;
-            plans.forEach(shop => {
-                shop.areas.forEach(area => areas.push(area.name))
-            });
-            //set делает объекты уникальными
-            return [...new Set(areas)];
-        },
-        getAllColors: function (data) {
-            const prices = [];
-            const plans = data && data.plans || this.data;
-            plans.forEach(shop => {
-                shop.areas.forEach(area => {
-                    area.prices.forEach(price => prices.push(price));
-                })
-            });
-            return [...new Set(prices)];
-        }
-    }
-    //?
-})(data);
-
-function initLists(data) {
-    const plansHtml = dataManipulator.getAllPlans(data);
-    const areasHtml = dataManipulator.getAllAreas(data);
-    const pricesHtml = dataManipulator.getAllPrices(data);
-    domManipulator.updateNode(plansList, domManipulator.generateList(plansHtml));
-    domManipulator.updateNode(areasList, domManipulator.generateList(areasHtml));
-    domManipulator.updateNode(pricesList, domManipulator.generateList(pricesHtml));
-
-}
-
-domManipulator.clearNodes(plansList, areasList, pricesList);
-initLists();
-//объекты, на к-ые можно подписаться
-const planObservable = Observable('');
-const areaObservable = Observable('');
-const priceObservable = Observable('');
-
-planObservable.subscribe((value) => {
-    //filteredData = JSON.parse(JSON.stringify(data));
-    filteredData = filter(filteredData, value, planObservable.value, areaObservable.value);
-    domManipulator.clearNodes(plansList, areasList, pricesList);
-    initLists(filteredData);
-});
-
-areaObservable.subscribe((value) => {
-    //filteredData = JSON.parse(JSON.stringify(data));
-    filteredData = filter(filteredData, planObservable.value, value, priceObservable.value);
-    domManipulator.clearNodes(plansList, areasList, pricesList);
-    initLists(filteredData);
-});
-
-priceObservable.subscribe((value) => {
-    //filteredData = JSON.parse(JSON.stringify(data));
-    filteredData = filter(filteredData, planObservable.value, areaObservable.value, value);
-    domManipulator.clearNodes(plansList, areasList, pricesList);
-    initLists(filteredData);
-});
-
-plansList.addEventListener('click', function (e) {
-        const plan = e.target.innerText;
-        planObservable.next(plan);
-        //?
-        e.stopImmediatePropagation();
-    }
-);
-
-areasList.addEventListener('click', function (e) {
-        const area = e.target.innerText;
-        areaObservable.next(area);
-        e.stopImmediatePropagation();
-    }
-);
-
-pricesList.addEventListener('click', function (e) {
-        const price = e.target.innerText;
-        priceObservable.next(price);
-        e.stopImmediatePropagation();
-    }
-);
-
-function clearFilters() {
-    planObservable.next('');
-    areaObservable.next('');
-    priceObservable.next('');
-    filteredData = JSON.parse(JSON.stringify(data));
-    initLists(data);
-}
-
-function filter(currentData, planFilter, areaFilter, priceFilter) {
-    let data = {};
-    if (currentData) data.plans = currentData.planss.map(plan => {
-        plan.areas = plan.areas.map(area => {
-            area.prices = area.prices.filter(price => {
-                return price.includes(priceFilter)
-            });
-            return area;
-        }).filter(area => {
-            return area.name.includes(areaFilter) && area.prices.length > 0;
-        });
-        return plan;
-    }).filter(plan => {
-        return plan.name.includes(planFilter)
     });
-    return data;
+    showPrice();
+}
+
+function changeStyleAreas(element) {
+    let areas = [document.getElementById("Kalininsky"),
+        document.getElementById("Central"),
+        document.getElementById("VO")];
+    areas.forEach(area => {
+        if (area === element) {
+            area.classList.add("btn-primary");
+            options["area"] = area.id;
+        } else {
+            area.classList.remove("btn-primary");
+        }
+    })
+    showPrice();
+}
+
+function changeStyleTime(element) {
+    let time = [document.getElementById("AM"),
+        document.getElementById("PM")];
+    time.forEach(time => {
+        if (time === element) {
+            time.classList.add("btn-primary");
+            options["time"] = time.id;
+        } else {
+            time.classList.remove("btn-primary");
+        }
+    });
+    showPrice();
+}
+
+function showPrice () {
+    let elem = document.getElementById("resPrice");
+    elem.value = prices["plan"][options["plan"]] + prices["area"][options["area"]] + prices["time"][options["time"]];
 }
